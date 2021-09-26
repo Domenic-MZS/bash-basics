e2209e2df27774b33b5f3a1ab5ddfa3496ccc68b
DMzS <domenicoenriquemeza20014@gmail.com>
2021-09-25 21:31:43 -0400

    feat(getopts): Add playground of named parameters

--- functionsparameter.sh
+++ functionsparameter.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# echo "Total vars: $#"
+# echo "First Argument: $1" # Postional args
+# echo "File Name: $0"
+# echo "Arguments: $@"
+
 # // Same for functions
-echo $1; 
-echo $2;
+# echo $1; 
+# echo $2;
+
+
+while getopts ":suid:-:" options; do
+  case $options in
+    s | S) echo "-s";;
+    u) echo "-u";;
+    i) echo "-i";;
+    d) echo "-d $OPTARG";;
+    -) 
+      case "${OPTARG}" in
+        dom) 
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          echo -e "CURR OPTARG: ${OPTARG}\nCURR VAL: ${val}\nNEXT ARG POS: ${OPTIND}"
+        ;;
+      esac
+      ;;
+    :) echo "No argument defined for -${OPTARG}";;
+    ?) echo "Invalid option: ${OPTARG}";;
+  esac;
+done;
